2d22e560d1927efc87410dd328386bb1e9cd0f63
Ziplaw <afrgplus@gmail.com>
2025-06-08 12:09:06 +0200

    Apply Rotations + Reset Transform (start, unfinished, need differentiation between ref var and value vars)

--- batch_export.py
+++ batch_export.py
@@ -21,6 +21,30 @@ class BatchExportPropertyGroup(bpy.types.PropertyGroup):
         description="Only export selected models",
         default=True,
         )
+    apply_rotations : bpy.props.BoolProperty(
+        name="Apply Rotation and Scale",
+        description="Apply Rotation and Scale before exporting",
+        default=False,
+        )
+    move_to_world_center : bpy.props.BoolProperty(
+        name="Reset Position",
+        description="Moves each object to 0,0,0 before exporting and back to their original position afterwards",
+        default=True,
+        )
+    export_settings : bpy.props.EnumProperty(
+        items=
+        [
+            ('UNITY', "Unity", "Unity's Export Settings",1),
+            ('UE5', "Unreal Engine 5", "Unreal Engine 5's Export Settings",2),
+        ],
+        name="Export Settings",
+        description="",
+        default='UNITY'
+        )
+        
+        
+    def draw(self,context):
+        self.layout.prop(self,"export_settings")
 
 class BatchExportOperator(bpy.types.Operator):
     """Tooltip"""
@@ -45,7 +69,29 @@ class BatchExportOperator(bpy.types.Operator):
         
         for object in selected_objects:
             object.select_set(True)
-            bpy.ops.export_scene.fbx(filepath= (f"{props.filepath}/{object.name}.fbx"),use_selection=True,apply_scale_options='FBX_SCALE_ALL',object_types={'MESH'},bake_space_transform=True, apply_unit_scale=True, add_leaf_bones=False, use_armature_deform_only=False, bake_anim=False,axis_forward='-Z', axis_up = 'Y')
+            
+            if props.apply_rotations:
+                bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
+                
+            original_position = object.location
+            
+            if props.move_to_world_center:
+                object.location = 0,0,0
+                print(original_position)
+                            
+            match props.export_settings:
+                case 'UNITY':
+                    bpy.ops.export_scene.fbx(filepath= (f"{props.filepath}/{object.name}.fbx"),use_selection=True,apply_scale_options='FBX_SCALE_ALL',object_types={'MESH'},bake_space_transform=True, apply_unit_scale=True, add_leaf_bones=False, use_armature_deform_only=False, bake_anim=False,axis_forward='-Z', axis_up = 'Y')
+                    
+                case 'UE5':
+                    bpy.ops.export_scene.fbx(filepath= (f"{props.filepath}/{object.name}.fbx"),use_selection=True,apply_scale_options='FBX_SCALE_ALL',object_types={'MESH'},bake_space_transform=True, apply_unit_scale=True, add_leaf_bones=False, use_armature_deform_only=False, bake_anim=False,axis_forward='-Z', axis_up = 'Y')
+                    
+                case _:
+                    raise Exception('Export Settings not Implemented')
+            
+            if props.move_to_world_center:
+                print(original_position)
+                object.location = original_position
         
         os.startfile(props.filepath)
         return {'FINISHED'}
@@ -65,6 +111,9 @@ class BatchExporter(bpy.types.Panel):
 
         layout.prop(props, "filepath")
         layout.prop(props, "only_selected")
+        layout.prop(props, "apply_rotations")
+        layout.prop(props, "move_to_world_center")
+        layout.prop(props, "export_settings")
         
         row = layout.row()
         op = row.operator("object.batch_export_operator")
